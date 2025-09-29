58b29680cb68147f91abcb6bd87382535373a9a0
minhquana <quann1906@gmail.com>
2025-09-29 18:54:28 +0700

    feat(database): Configure alembic for data migration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -2,10 +2,16 @@ from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+from src.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    "postgresql://postgres_admin:postgres_password@localhost:5432/chat_conversation_db",
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -16,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
