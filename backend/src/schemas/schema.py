f2ea8c8a5fc2810819631a7cae75455780097938
minhquana1906 <quann1906@gmail.com>
2025-11-03 02:51:53 +0700

    refactor: Remove celery task for RAG workflow, call function directly; update chainlit UI

--- backend/src/schemas/schema.py
+++ backend/src/schemas/schema.py
@@ -2,22 +2,22 @@ from typing import Dict, List, Optional
 from pydantic import BaseModel, Field
 
 
-class CompleteRequest(BaseModel):
+# class CompleteRequest(BaseModel):
 
-    bot_id: str = Field(
-        default="Meddy",
-        description="The ID of the bot to use for completion.",
-    )
-    user_id: str = Field(
-        default="user_1", description="The ID of the user making the request."
-    )
-    user_message: str = Field(..., description="The message from the user.")
-    is_sync_request: Optional[bool] = Field(
-        False, description="Whether the request is synchronous or asynchronous."
-    )
-    metadata: Optional[Dict] = Field(
-        None, description="Additional metadata for the request."
-    )
+#     bot_id: str = Field(
+#         default="Meddy",
+#         description="The ID of the bot to use for completion.",
+#     )
+#     user_id: str = Field(
+#         default="user_1", description="The ID of the user making the request."
+#     )
+#     user_message: str = Field(..., description="The message from the user.")
+#     is_sync_request: Optional[bool] = Field(
+#         False, description="Whether the request is synchronous or asynchronous."
+#     )
+#     metadata: Optional[Dict] = Field(
+#         None, description="Additional metadata for the request."
+#     )
 
 
 class RAGQueryRequest(BaseModel):
